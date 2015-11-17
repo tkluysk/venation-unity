ef95d7fc108271eff41173175a52806493278b30
Tom Kluyskens <tom@mattermachine.com>
2015-11-17 18:06:53 +1300

    draw auxins

--- Assets/SimpleRenderer.cs
+++ Assets/SimpleRenderer.cs
@@ -18,7 +18,7 @@ class SimpleRenderer
 		PVector p;
 		List<VeinNode> veinNodes = va.getVeinNodes();
 		
-		Debug.Log ( "drawing nodes " + veinNodes.Count );
+//		Debug.Log ( "drawing nodes " + veinNodes.Count );
         
         lineMaterial.SetPass ( 0 );
 		GL.PushMatrix();
@@ -27,12 +27,21 @@ class SimpleRenderer
 		GL.Color ( Color.red );
 		
 		foreach ( VeinNode veinNode in veinNodes ) {
-			p = veinNode.getPositionRef() * size;
-			Debug.Log ( "drawing node " + p.x + " " + p.y );
-			MiscUtil.GLDrawCross ( p, 2.0f );
+			p = veinNode.getPosition() * size;
+//			Debug.Log ( "drawing node " + p.x + " " + p.y );
+			MiscUtil.GLDrawCircle ( p, 2.0f );
 		}
+		
+		GL.Color ( Color.black );
 
-		GL.End();
+		foreach ( Auxin auxin in va._auxins ) {
+			p = auxin.getPosition() * size;
+//			Debug.Log ( "drawing auxin " + p.x + " " + p.y );
+			MiscUtil.GLDrawCross ( p, 1.0f );
+//			MiscUtil.GLDrawDot ( p );
+        }
+        
+        GL.End();
 		GL.PopMatrix();
     }
 }
